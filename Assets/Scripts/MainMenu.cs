af40dc40906547f25056c34b54b9fa4abe842c4f
Tanner May <tamfrb@mst.edu>
2019-11-10 01:26:21 -0600

    Main menu commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public bool isStart;
+	public bool isCredit;
+	public bool isQuit;
+	
+	void OnMouseUp()
+	{
+		if(isStart)
+		{
+			GetComponent<Renderer>().material.color=Color.cyan;
+			Application.LoadLevel(1);
+		}
+		if(isCredit)
+		{
+			Application.LoadLevel(2);
+		}
+		if(isQuit)
+		{
+			Application.Quit();
+		}
+	}
+}
